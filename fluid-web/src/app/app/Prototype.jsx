a5e567eae12307c38ff40ea63943a7d42e785170
mikaelcharbonneau <153523692+mikaelcharbonneau@users.noreply.github.com>
2026-07-29 22:47:22 -0400

    Add pictorial logo references to logo type cards

--- fluid-web/src/app/app/Prototype.jsx
+++ fluid-web/src/app/app/Prototype.jsx
@@ -1405,7 +1405,7 @@ const LOGO_TYPE_OPTIONS = [
 
 const ALogoTypeCard = ({ type, index, selected, onClick }) => {
   const [hot, setHot] = React.useState(false);
-  const refs = ['wordmark', 'lettermark'].includes(type.id) ? logoReferencesFor(type.id) : [];
+  const refs = logoReferencesFor(type.id);
   return (
     <button
       type="button"
